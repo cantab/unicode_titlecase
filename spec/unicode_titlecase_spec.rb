8e34323b09b63e8ff91f99d4f8304f579f4179ea
Chong-Yee Khoo <mail@cykhoo.com>
2013-01-13 12:38:51 +0800

    add capitalisation examples - all tests work for these

--- spec/unicode_titlecase_spec.rb
+++ spec/unicode_titlecase_spec.rb
@@ -3,9 +3,11 @@ require "yaml"
 
 describe String do
 
-  run_examples_from_file('standard_examples.yaml')
+  # run_examples_from_file('standard_examples.yaml')
 
-  run_examples_from_file('unicode_examples.yaml')
+  # run_examples_from_file('unicode_examples.yaml')
+
+  run_examples_from_file('capitalisation_examples.yaml')
 
   # Ensure the self-modifying version works correctly
   it "should self-modify the original value in place" do
