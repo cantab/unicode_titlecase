518f5d6a266d96ec33f9f2cf76d55f199c0f51bc
Chong-Yee Khoo <mail@cykhoo.com>
2013-11-01 12:23:05 +0800

    move examples to their own directories

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,12 @@
 #encoding: UTF-8
 
 require 'unicode_titlecase'
+require 'yaml'
 require 'rspec'
 
 def run_examples_from_file(filename)
 
-  File.open(File.join(File.dirname(__FILE__), *filename)) do |file|
+  File.open(File.join(File.dirname(__FILE__), "examples/", *filename)) do |file|
     examples = YAML.load(file)
     examples["should_pass"].each do |e|
       it "#{e["example"]} when titlecased is the expected value (#{e["expect"]})" do
--- spec/unicode_titlecase_spec.rb
+++ spec/unicode_titlecase_spec.rb
@@ -1,29 +1,49 @@
 require 'spec_helper'
-require "yaml"
 
 describe String do
 
-  run_examples_from_file('standard_examples.yaml')
-
-  run_examples_from_file('unicode_examples.yaml')
-
-  run_examples_from_file('capitalisation_examples.yaml')
+  let(:string) { 'the rain in spain' }
 
   describe "unicode_titlecase method" do
+
+    it "returns a title cased version of the string" do
+      expect(string.unicode_titlecase).to eq('The Rain in Spain')
+    end
+
     it "does not modify the original string" do
-      string = 'a complex thing'
       string.unicode_titlecase
-      expect(string).to eq('a complex thing')
-      expect(string).to_not eq('A Complex Thing')
+      expect(string).to eq('the rain in spain')
     end
   end
 
   describe "unicode_titlecase! method" do
+
+    it "returns a title cased version of the string" do
+      expect(string.unicode_titlecase!).to eq('The Rain in Spain')
+    end
+
     it "self-modifies the original value in place" do
-      string = 'a complex thing'
       string.unicode_titlecase!
-      expect(string).to eq('A Complex Thing')
-      expect(string).to_not eq('a complex thing')
+      expect(string).to eq('The Rain in Spain')
+    end
+  end
+
+  describe "examples" do
+
+    describe "bigword examples" do
+      run_examples_from_file('bigword_examples.yaml')
+    end
+
+    describe "capitalisation examples" do
+      run_examples_from_file('capitalisation_examples.yaml')
+    end
+
+    describe "standard examples" do
+      run_examples_from_file('standard_examples.yaml')
+    end
+
+    describe "unicode examples" do
+      run_examples_from_file('unicode_examples.yaml')
     end
   end
 end
